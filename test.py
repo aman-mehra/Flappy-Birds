f31d15fe97dfb522cb23dd7ca5d84961ef18475c
Chirag Jain <jainchg@gmail.com>
2019-01-20 17:26:37 +0530

    modified assets, corrected code

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ pygame.init()
 display=pygame.display.set_mode((1000,700))
 pygame.display.set_caption("FLAPPY BIRD")
 display.fill((0,0,0))
-img=pygame.image.load("Bird1.png")
+img=pygame.image.load("assets/images/Bird1.png")
 img=pygame.transform.scale(img,(50,50))
 display.blit(img,(0,0))
 
